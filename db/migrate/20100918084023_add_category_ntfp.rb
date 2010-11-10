bf7192dae453293bdb7790b5c431a0a5fa2a4d68
HariKrishnan <harikrishnan83@gmail.com>
2010-11-10 21:58:43 +0530

    Remving Non Timber Forest Produce from categories

--- db/migrate/20100918084023_add_category_ntfp.rb
+++
@@ -1,9 +0,0 @@
-class AddCategoryNtfp < ActiveRecord::Migration
-  def self.up
-    Category.create(:name=>"Non Timber Forest Produce")
-  end
-
-  def self.down
-	Category.destroy(:name=>"Non Timber Forest Produce")
-  end
-end
